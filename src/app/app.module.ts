2e7f588c07753dd69f57a50571bf0ebcc49211c7
Anamika Singh <anasingh1593@gmail.com>
2018-02-14 23:40:59 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
+import { NewComponentComponent } from './new-component/new-component.component';
+import { FilterPipe} from './filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewComponentComponent,
+	FilterPipe
   ],
   imports: [
     BrowserModule
+	FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/new-component/new-component.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-component',
+  templateUrl: './new-component.component.html',
+  styleUrls: ['./new-component.component.css']
+})
+export class NewComponentComponent {
+
+ /* constructor() { }
+
+  ngOnInit() {
+  }*/
+  name = 'Angular';
+characters = [
+    'Finn the human',
+    'Jake the dog',
+    'Princess bubblegum',
+    'Lumpy Space Princess',
+    'Beemo1',
+    'Beemo2'
+  ]
+}
