8424b3c340f49aa768b2b0d21d925213291aee6f
singhanamika <anasingh1593@gmail.com>
2018-02-20 20:45:39 -0500

    Second Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
-
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { NgDatepickerModule } from 'ng2-datepicker';
 import { AppComponent } from './app.component';
 import { NewComponentComponent } from './new-component/new-component.component';
-import { FilterPipe} from './filter.pipe';
+
+/*import { FilterPipe} from './filter.pipe';*/
 
 @NgModule({
   declarations: [
     AppComponent,
     NewComponentComponent,
-	FilterPipe
+	
   ],
   imports: [
-    BrowserModule
-	FormsModule
+    BrowserModule,
+	FormsModule,
+	Ng2SearchPipeModule,
+	NgDatepickerModule
+	
+	
+
   ],
   providers: [],
   bootstrap: [AppComponent]
