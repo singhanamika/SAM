79bd7b7c3587cf588266749b777124ee48f68310
singhanamika <anasingh1593@gmail.com>
2018-02-20 20:48:25 -0500

    Second Commit

--- src/app/new-component/new-component.component.ts
+++ src/app/new-component/new-component.component.ts
@@ -1,23 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
+
 @Component({
   selector: 'app-new-component',
   templateUrl: './new-component.component.html',
   styleUrls: ['./new-component.component.css']
 })
 export class NewComponentComponent {
+	
 
- /* constructor() { }
+ 
 
-  ngOnInit() {
-  }*/
-  name = 'Angular';
-characters = [
-    'Finn the human',
-    'Jake the dog',
-    'Princess bubblegum',
-    'Lumpy Space Princess',
-    'Beemo1',
-    'Beemo2'
-  ]
+  
+  welcome: string;
+    games : [{
+		index: number,
+        game: string,
+        platform : string,
+        release : string
+    }];
+	
+ 
+    
+    constructor(){
+        this.welcome = "Search Criteria"
+
+        this.games = [{
+			index: 1,
+            game : "Deus Ex: Mankind Divided",
+            platform: " Xbox One, PS4, PC",
+            release : "August 23"
+        },
+        {
+			index: 2,
+            game : "Hue",
+            platform: " Xbox One, PS4, Vita, PC",
+            release : "August 23"
+        },
+        {
+			index: 3,
+            game : "The Huntsman: Winter's Curse",
+            platform: "PS4",
+            release : "August 23"
+        },
+        {
+			index: 4,
+            game : "The Huntsman: Winter's Curse",
+            platform: "PS4",
+            release : "August 23"
+        }]
+    };
+/*ngOnInit() {
+	
+  }	*/
 }
