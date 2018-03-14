1c879bf13c32c7ebacee468eb3be41c4271b3ebc
singhanamika <anasingh1593@gmail.com>
2018-03-14 03:17:19 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,46 @@
+//import { FormsModule, ReactiveFormModule } from '@angular/forms';
+//import { OverlayModule } from "angular-io-overlay";
+//import { DatePickerModule } from "angular-io-datepicker/src/datepicker/index";
+
+//import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FilterPipe } from 'app/filter.pipe'  
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule }   from '@angular/forms';
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
-import { NgDatepickerModule } from 'ng2-datepicker';
+import {FormsModule} from '@angular/forms';
+//import { NgDatepickerModule } from 'ng2-datepicker';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { NewComponentComponent } from './new-component/new-component.component';
+import { CourseComponent } from './course/course.component';
+import { EmailService } from 'app/email.service';
+//import { MaterialModule } from '@angular/material';
+//import { DatePickerModule } from 'angular-material-datepicker';
+import { MaterialModule, MdDatepickerModule, MdNativeDateModule } from '@angular/material';
 
-/*import { FilterPipe} from './filter.pipe';*/
-
+import 'hammerjs';
 @NgModule({
   declarations: [
     AppComponent,
-    NewComponentComponent,
-	
+    CourseComponent,
+	FilterPipe
   ],
   imports: [
     BrowserModule,
 	FormsModule,
-	Ng2SearchPipeModule,
-	NgDatepickerModule
-	
-	
-
+	//ReactiveFormModule,
+   // OverlayModule,
+    //DatePickerModule,
+	//NgDatepickerModule
+	//MaterialModule.forRoot(),
+        //DatePickerModule
+	BrowserAnimationsModule,
+	MaterialModule,            // <----- this module will be deprecated in the future version.
+    MatDatepickerModule,        // <----- import(must)
+    MatNativeDateModule    
   ],
-  providers: [],
+  providers: [
+ 
+  EmailService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
